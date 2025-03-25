b468767b2f1e4509e527e95f47f4c234925cf0ea
Patcharada37778 <patcharada37778@gmail.com>
2025-03-26 06:04:21 +0700

    CarCare v 1.0.9

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-=======
-"use client";
-
->>>>>>> e070a46bfb3b7bc5a728a1e18de5795df294ed9d
 import Image from "next/image";
 import TopMenuItem from "./TopMenuItem";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { Link } from "@mui/material";
-<<<<<<< HEAD
 import styles from "./topmenu.module.css";
-=======
-import styles from "./topmenu.module.css"; // Import the styles
->>>>>>> e070a46bfb3b7bc5a728a1e18de5795df294ed9d
 
 export default async function TopMenu() {
   const session = await getServerSession(authOptions);
@@ -27,44 +18,18 @@ export default async function TopMenu() {
         height={0}
         sizes="100vh"
       />
-<<<<<<< HEAD
-=======
-
->>>>>>> e070a46bfb3b7bc5a728a1e18de5795df294ed9d
       <TopMenuItem title="Select Car" pageRef="/car" />
       <TopMenuItem title="Home" pageRef="/" />
 
-      <div className={`${styles.rightSection} flex flex-row absolute right-0 h-full`}>
+      <div className={`flex flex-row absolute right-0 h-full`}>
         <TopMenuItem title="Cart" pageRef="/cart" />
         {session ? (
-          <Link href="/api/auth/signout">
-<<<<<<< HEAD
-            <div className={`mt-3 ${styles.itemcontainer}`}>Sign-Out</div>
-=======
-            <div className={styles.itemcontainer}>
-              Sign-Out {/*session.user?.name*/}
-            </div>
->>>>>>> e070a46bfb3b7bc5a728a1e18de5795df294ed9d
-          </Link>
+          <TopMenuItem title="Sign-Out" pageRef="/api/auth/signout" />
         ) : (
           <>
-            <Link href="/register">
-<<<<<<< HEAD
-              <div className={`${styles.itemcontainer} mt-3`}>Register</div>
-            </Link>
-            <Link href="/api/auth/signin">
-              <div className={`${styles.itemcontainer} mt-3`}>Sign-In</div>
-=======
-              <div className={styles.itemcontainer}> {/* Apply itemcontainer style */}
-                Register
-              </div>
-            </Link>
-            <Link href="/api/auth/signin">
-              <div className={styles.itemcontainer}> {/* Apply itemcontainer style */}
-                Sign-In
-              </div>
->>>>>>> e070a46bfb3b7bc5a728a1e18de5795df294ed9d
-            </Link>
+          <TopMenuItem title="Register" pageRef="/register" />
+          <TopMenuItem title="Sign-in" pageRef="/api/auth/signin" />
+
           </>
         )}
       </div>
